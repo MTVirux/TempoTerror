8346bd19741f5fe6daf1dfa955ecbef28e3b88a6
MTVirux <joaofraga96@outlook.com>
2026-04-09 16:21:20 +0100

    Unify Row Layout config

--- TempoTerror/Gui/ConfigWindow/ConfigWindow.cs
+++ TempoTerror/Gui/ConfigWindow/ConfigWindow.cs
@@ -1,10 +1,12 @@
 namespace TempoTerror.Gui.ConfigWindow;
 
 using System;
+using System.Collections.Generic;
 using System.Numerics;
 using Dalamud.Bindings.ImGui;
 using Dalamud.Interface.Windowing;
 using Dalamud.Plugin;
+using TempoTerror.Models;
 
 public sealed class ConfigWindow : Window, IDisposable
 {
@@ -53,37 +55,6 @@ public sealed class ConfigWindow : Window, IDisposable
             }
         }
 
-        if (ImGui.CollapsingHeader("Action Filters", ImGuiTreeNodeFlags.DefaultOpen))
-        {
-            var showGcd = this.config.ShowGcd;
-            if (ImGui.Checkbox("Show GCD", ref showGcd))
-            {
-                this.config.ShowGcd = showGcd;
-                changed = true;
-            }
-
-            var showOgcd = this.config.ShowOgcd;
-            if (ImGui.Checkbox("Show oGCD", ref showOgcd))
-            {
-                this.config.ShowOgcd = showOgcd;
-                changed = true;
-            }
-
-            var showPet = this.config.ShowPet;
-            if (ImGui.Checkbox("Show Pet", ref showPet))
-            {
-                this.config.ShowPet = showPet;
-                changed = true;
-            }
-
-            var showAutoAttack = this.config.ShowAutoAttack;
-            if (ImGui.Checkbox("Show Auto-Attack", ref showAutoAttack))
-            {
-                this.config.ShowAutoAttack = showAutoAttack;
-                changed = true;
-            }
-        }
-
         if (ImGui.CollapsingHeader("Display", ImGuiTreeNodeFlags.DefaultOpen))
         {
             var showCastBars = this.config.ShowCastBars;
@@ -101,7 +72,119 @@ public sealed class ConfigWindow : Window, IDisposable
             }
         }
 
+        if (ImGui.CollapsingHeader("Row Layout", ImGuiTreeNodeFlags.DefaultOpen))
+        {
+            ImGui.TextUnformatted("Toggle visibility, reorder rows, and adjust height.");
+            ImGui.Spacing();
+
+            // Deduplicate row order in case saved config has duplicates
+            var order = this.config.RowOrder;
+            var seen = new HashSet<ActionType>();
+            for (var j = order.Count - 1; j >= 0; j--)
+            {
+                if (!seen.Add(order[j]))
+                    order.RemoveAt(j);
+            }
+
+            var heights = this.config.RowHeights;
+            int? swapFrom = null;
+            int? swapTo = null;
+
+            for (var i = 0; i < order.Count; i++)
+            {
+                var rowType = order[i];
+                var label = GetRowLabel(rowType);
+
+                ImGui.PushID(i);
+
+                // Visibility checkbox
+                var visible = this.GetRowVisible(rowType);
+                if (ImGui.Checkbox("##visible", ref visible))
+                {
+                    this.SetRowVisible(rowType, visible);
+                    changed = true;
+                }
+
+                ImGui.SameLine();
+
+                // Move up button
+                var canUp = i > 0;
+                if (!canUp) ImGui.BeginDisabled();
+                if (ImGui.ArrowButton("##up", ImGuiDir.Up))
+                {
+                    swapFrom = i;
+                    swapTo = i - 1;
+                }
+
+                if (!canUp) ImGui.EndDisabled();
+
+                ImGui.SameLine();
+
+                // Move down button
+                var canDown = i < order.Count - 1;
+                if (!canDown) ImGui.BeginDisabled();
+                if (ImGui.ArrowButton("##down", ImGuiDir.Down))
+                {
+                    swapFrom = i;
+                    swapTo = i + 1;
+                }
+
+                if (!canDown) ImGui.EndDisabled();
+
+                ImGui.SameLine();
+                ImGui.TextUnformatted(label);
+                ImGui.SameLine();
+
+                ImGui.SetNextItemWidth(120);
+                if (!heights.TryGetValue(rowType, out var h))
+                    h = 1.0f;
+
+                if (ImGui.SliderFloat("##height", ref h, 0.25f, 3.0f, "%.2f"))
+                {
+                    heights[rowType] = h;
+                    changed = true;
+                }
+
+                ImGui.PopID();
+            }
+
+            if (swapFrom is not null && swapTo is not null)
+            {
+                (order[swapFrom.Value], order[swapTo.Value]) = (order[swapTo.Value], order[swapFrom.Value]);
+                changed = true;
+            }
+        }
+
         if (changed)
             this.pluginInterface.SavePluginConfig(this.config);
+    }
+
+    private static string GetRowLabel(ActionType type) => type switch
+    {
+        ActionType.AutoAttack => "Auto-Attack",
+        ActionType.Gcd => "GCD",
+        ActionType.Ogcd => "oGCD",
+        ActionType.Pet => "Pet",
+        _ => "Unknown",
+    };
+
+    private bool GetRowVisible(ActionType type) => type switch
+    {
+        ActionType.Gcd => this.config.ShowGcd,
+        ActionType.Ogcd => this.config.ShowOgcd,
+        ActionType.AutoAttack => this.config.ShowAutoAttack,
+        ActionType.Pet => this.config.ShowPet,
+        _ => true,
+    };
+
+    private void SetRowVisible(ActionType type, bool visible)
+    {
+        switch (type)
+        {
+            case ActionType.Gcd: this.config.ShowGcd = visible; break;
+            case ActionType.Ogcd: this.config.ShowOgcd = visible; break;
+            case ActionType.AutoAttack: this.config.ShowAutoAttack = visible; break;
+            case ActionType.Pet: this.config.ShowPet = visible; break;
+        }
     }
 }
